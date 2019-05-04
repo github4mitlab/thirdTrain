a4e5c8bee6cba30ddbda9d4f9ecab72a174af165
cio <cio@mitlab.kr>
2019-05-05 07:54:04 +0900

    Request Products Info through Detailed Query

--- api/routes/products.js
+++ api/routes/products.js
@@ -23,6 +23,33 @@ router.get("/", (req, res) => {
     });
 });
 
+
+// Request Products Info through Detailed Query
+router.get('/:productId', (req, res) => {
+    const id = req.params.productId;
+    productModel.findById(id)
+        .exec()
+        .then( doc => {
+            console.log("Quering DB", doc);
+            if (doc) {
+                res.status(200).json({
+                    productInfo: doc
+                });
+            } else {
+                res.status(400).json({
+                    msg: "해당 정보 없음"
+                });
+            }
+        })
+        .catch( err => {
+            console.log(err);
+            res.status(200).json({
+                error: err
+            });
+        });
+        
+});
+
 // router.get("/:productId", (req, res) => {
 //   const id = req.params.productId;
 //   if (id === "special") {
