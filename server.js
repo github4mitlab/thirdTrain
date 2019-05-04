37d5bc09a4a1427781d0c583ac0d9ffc8cc969b0
cio <cio@mitlab.kr>
2019-05-04 23:54:10 +0900

    에러핸들링 구현

--- server.js
+++ server.js
@@ -5,13 +5,34 @@ const express = require('express');
 
 const app = express();
 
+//에러핸들링
+const morgan = require('morgan');
+
+
 //라우터 구현
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+//에러 핸들러 구현
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);    
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            msg: error.message
+        }
+    });
+});
 
 
 
