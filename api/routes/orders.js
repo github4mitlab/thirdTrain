e7fdffb649d284e80741830beabaf34889f4381d
cio <cio@mitlab.kr>
2019-05-04 23:44:30 +0900

    CRUD RESTufl API 적용

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,5 +7,40 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:orderId', (req, res) => {
+    const id = req.params.orderId;
+    if (id !== '1234') {
+        res.status(200).json({
+            orders_message: "잘못된 아이디"
+        });
+    } else {
+        res.status(200).json({
+            orders_message: "정확한 아이디",
+            id : id
+        });
+
+    }
+
+});
+
+
+router.post('/', (req, res) => {
+    res.status(200).json({
+        message: "POST / orders.js"
+    });
+});
+
+router.put('/', (req, res) => {
+    res.status(200).json({
+        message: "PUT / orders.js"
+    });
+});
+
+router.delete('/', (req, res) => {
+    res.status(200).json({
+        message: "DELETE / orders.js"
+    });
+});
+
 module.exports = router;
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -3,7 +3,44 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.status(200).json({
-        product_message: "GET Product"
+        message: "GET Product"
+    });
+});
+
+router.get('/:productId', (req, res) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            id_check_msg: "마즘", 
+            id : id
+        });        
+    } else {
+        res.status(200).json({
+            id_check_msg: "틀림"
+        });
+    }
+});
+
+
+// post router
+router.post('/', (req, res) => {
+    res.status(200).json({
+        msg: "post / orders.js"
+    });
+});
+
+// put router
+router.put('/', (req, res) => {
+    res.status(200).json({
+        msg: "put / orders.js"
+    });
+});
+
+
+// delete router
+router.delete('/',(req, res) => {
+    res.status(200).json({
+        msg: "delete / order.js"
     });
 });
 
