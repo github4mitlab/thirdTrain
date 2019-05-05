d7d750b28bd4cba40bcc35a04bd6a89f9ecb1296
surfaceMay <may1516@gmail.com>
2019-05-05 16:50:54 +0900

    resiter order 구현

--- api/models/order.js
+++ api/models/order.js
@@ -1,18 +1,30 @@
-const mongooes = require('mongoose');
+const mongoose = require("mongoose");
 
-// Add Order Model
-const orderSchema = mongooes.Schema({
-    _id: mongooes.Schema.Types.ObjectId,
-    product: {
-        type: mongooes.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: true
-    },
-    quantity: {
-        type: Number,
-        default: 1
-    }
+const orderSchema = mongoose.Schema({
+
+    _id: mongoose.Schema.Types.ObjectId,
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, default: 1 }
 
 });
 
-module.exports = mongooes.model('Order', orderSchema);
+
+
+// const mongooes = require('mongoose');
+
+// // Add Order Model
+// const orderSchema = mongooes.Schema({
+//     _id: mongooes.Schema.Types.ObjectId,
+//     product: {
+//         type: mongooes.Schema.Types.ObjectId,
+//         ref: 'Product',
+//         required: true
+//     },
+//     quantity: {
+//         type: Number,
+//         default: 1
+//     }
+
+// });
+
+module.exports = mongoose.model('Order', orderSchema);
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const orderModel = require('../models/order');
+const productModel = require('../models/products');
+
 
 router.get('/', (req, res) => {
     res.status(200).json({
@@ -24,10 +29,85 @@ router.get('/:orderId', (req, res) => {
 });
 
 
+// Order Register
+// root: /orders/
+
+
 router.post('/', (req, res) => {
-    res.status(200).json({
-        message: "POST / orders.js"
+    productModel.findById(req.body.productId)
+    .then(product => {
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+        const order = new orderModel({
+            _id: mongoose.Types.ObjectId(),
+            quantity: req.body.quantity,
+            product: req.body.productId
+        });
+        return order.save();
+    })
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "Order stored",
+            createdOrder: {
+                _id: result._id,
+                product: result.product,
+                quantity: result.quantity
+            },
+            request: {
+                type: "GET",
+                url: "http://localhost:3000/orders/" + result._id
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
+    // productModel
+    //     .findById(req.body.productId)
+    //     .then( product => {
+    //         // 제품이 없는 경우 처리
+    //         if(!product) {
+    //             return res.status(404).json({
+    //                 ord_msg: "Product Not Found."
+    //             });
+    //         }
+
+    //         //제품이 있는 경우
+    //         const order = new orderModel({
+    //             _id: mongoose.Types.ObjectId(),
+    //             product: req.body.productId,
+    //             quantity: req.body.quantity
+    //         });
+    //         return order.save();
+    //     })
+    //     .then( result => {
+    //         console.log(result);
+    //         res.status(201).json({
+    //             ord_msg:"Order Stored",
+    //             createdOrder: {
+    //                 _id: result._id,
+    //                 product: result.product,
+    //                 quantity:result.quantity
+    //             },
+    //             request: {
+    //                 type: 'GET',
+    //                 url: "http://localshot:3000/orders/" + result._id
+    //             }
+    //         });
+    //     })
+    //     .catch( err => {
+    //         console.log(err);
+    //         res.status(500).json({
+    //             ord_err: err
+    //         });
+    //     });
 });
 
 router.put('/', (req, res) => {
