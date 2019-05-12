d77cbb410a0f39ea73b4bea06a45692a2059d66c
surfaceMay <may1516@gmail.com>
2019-05-12 16:04:13 +0900

    Add bcrypt

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,38 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userModel = require('../models/user');
+const userModel = require("../models/user");
 
-// Register User 
+// Register User
 // 아래의 '/singup 이라는 코드는 users 경로에 추가적으로 서브 경로를 만든 것이다.
-router.post('/signup', (req,res) => {
-    // res.json({
-    //     msg: "Create"
-    // });
-    const user = new userModel({
+router.post("/signup", (req, res) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      return res.status(500).json({
+        usr_msg: err
+      });
+    } else {
+      const user = new userModel({
         _id: new mongoose.Types.ObjectId(),
         email: req.body.email,
-        password: req.body.password
-    });
-    
-    console.log(user);
+        password: hash
+      });
 
-    user
+      console.log(user);
+
+      user
         .save()
         .then(result => {
-            console.log(result);
-            res.status(200).json({
-                message: 'User created'
-            });
+          console.log(result);
+          res.status(200).json({
+            message: "User created"
+          });
         })
         .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
+          console.log(err);
+          res.status(500).json({
+            error: err
+          });
         });
-
+    }
+  });
 });
 
 module.exports = router;
